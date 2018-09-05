106f6396d80b948e4c438f864791bc417771a15f
ahmed-abdellatif <ahmedma@email.sc.edu>
2018-09-05 01:36:45 -0400

    reading files

---
+++ ReadingFiles/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReadingFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            // Read each line in the text line by line and store the lines in an array
+            string [] lines = System.IO.File.ReadAllLines(@"/Users/owner-name/Desktop/paper.txt");
+
+            System.Console.WriteLine("This text file read = ");
+            // Display the contents of the file and seperate paragraphs with a tab
+            foreach(string line in lines) {
+                Console.WriteLine("\t" + line);
+            }
+            Console.WriteLine("Press any key to exit");
+            System.Console.ReadKey();
+        }
+    }
+}
